39e8f957f813d473deb1355e20a27eee333259c4
Sakshi <sakshiparashar2910@gmail.com>
2026-04-19 15:59:28 +0530

    Feat(Backend): add the folder structure and connect database

---
+++ Backend/src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./database/db');
+
+const app = express();
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+})
