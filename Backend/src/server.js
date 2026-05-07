11af3eca3a0090fa1cee1e27820e77a68d3802b7
Sakshi <sakshiparashar2910@gmail.com>
2026-05-07 22:54:59 +0530

    handle cors connection

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -9,8 +9,19 @@ const cors = require('cors');
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://code-deja-vu.vercel.app'
+];
+
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 }));
 
